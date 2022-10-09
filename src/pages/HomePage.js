eb43b9e959a0b0770718a25daa524c66a3e2381a
seppe <r0888765@student.thomasmore.be>
2022-10-09 16:14:40 +0200

    tussenresultaat-1 no more broken images

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 function HomePage (){
     return (
         <Container style={{margin:0, height:"100%"}} >
-                <p> test 4</p>
+                <p> test 5</p>
                 <h1>Welcome to the convention</h1>
             <Link to={"/signup"}><Button> sign up </Button></Link>
            <Link to={"/login"}><Button> log in </Button></Link>
