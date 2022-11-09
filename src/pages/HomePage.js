ed315303cdee3f99a1e92dc5ff12062ccf2031ad
seppe <r0888765@student.thomasmore.be>
2022-11-09 23:20:20 +0100

    home page small edit.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -23,15 +23,15 @@ function HomePage(props) {
                 <div style={{display: "flex", direction: "row", justifyContent: "center"}}>
                     <div style={{backgroundColor: "#060b26", borderRadius: "5px", marginRight: "20px"}}
                          className="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
-                        <h2 className="mb-3 text-white">New to the Valorant?</h2>
-                        <h6 className="mb-4 text-white">join the Valorant community now!</h6>
+                        <h2 className="mb-3 text-white">New to the Convention?</h2>
+                        <h6 className="mb-4 text-white">join the Convention now!</h6>
                         <Link to={"/signup"}><Button style={{marginBottom: "20px", backgroundColor: "#6B8BF5"}}> sign
                             up </Button></Link>
                     </div>
                     <div style={{backgroundColor: "#060b26", borderRadius: "5px"}}
                          className="d-flex flex-column justify-content-center ml-lg-auto mr-lg-5 col-lg-5 col-md-6 col-12">
                         <h2 className="mb-3 text-white">Already a Member?</h2>
-                        <h6 className="mb-4 text-white">join the Valorant community now!</h6>
+                        <h6 className="mb-4 text-white">Log in to the convention!</h6>
                         <Link to={"/login"}><Button style={{marginBottom: "20px", backgroundColor: "#6B8BF5"}}> log
                             in </Button></Link>
                     </div>
